f16f172c7b88fc056d4d71ada308ab727b41c12f
SAlNT <kirilhmel13@gmail.com>
2022-05-15 23:11:51 +0300

    start writing normal sudoku solver

--- 3kyu/sudoku_solver.py
+++ 3kyu/sudoku_solver.py
@@ -1,85 +1,119 @@
-import random
-import copy
-
-digits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-
-def check_puzzle(puzzle, point):
-    global counter
+def check_puzzle(v, point):
     p_i, p_j = point
-    p_v = puzzle[p_i][p_j]
-
+    p_v = v
     if [puzzle[p_i][j] for j in range(9)].count(p_v) > 1 or [puzzle[i][p_j] for i in range(9)].count(p_v) > 1:
         return False
 
-    p_square = []
-    l_border = (p_i // 3) * 3
-    r_border = l_border + 3
-    u_border = (p_j // 3) * 3
-    d_border = u_border + 3
-    for i in range(9):
-        for j in range(9):
-            if l_border <= j < r_border and u_border <= i < d_border:
-                p_square.append((i, j))
-    if [puzzle[p[0]][p[1]] for p in p_square].count(p_v) > 1:
+    # проверка на наличие такой цифры в квадрате кандидата
+    if [puzzle[p[0]][p[1]] for p in
+        [(i, j) for i in range(9) for j in range(9) if
+         (p_i // 3) * 3 <= j < (p_i // 3) * 3 + 3 and (p_j // 3) * 3 <= i < (p_j // 3) * 3 + 3]].count(p_v) > 1:
         return False
 
     return True
 
 
-def get_zeros(puzzle):
-    result = []
-    for i in range(9):
-        for j in range(9):
-            if puzzle[i][j] == 0:
-                result.append((i, j))
-    return result
-
-
-puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-          [6, 0, 0, 1, 9, 5, 0, 0, 0],
-          [0, 9, 8, 0, 0, 0, 0, 6, 0],
-          [8, 0, 0, 0, 6, 0, 0, 0, 3],
-          [4, 0, 0, 8, 0, 3, 0, 0, 1],
-          [7, 0, 0, 0, 2, 0, 0, 0, 6],
-          [0, 6, 0, 0, 0, 0, 2, 8, 0],
-          [0, 0, 0, 4, 1, 9, 0, 0, 5],
-          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
-
-
-def solve_sudoku(puzzle, new_positions, new_values, counter):
-    for v in range(new_values[counter], 10):
-        puzzle[new_positions[counter][0]][new_positions[counter][1]] = v
-        if check_puzzle(puzzle, (new_positions[counter][0], new_positions[counter][1])):
-            new_values[counter] = v
-            counter += 1
-            break
-        else:
-            puzzle[new_positions[counter][0]][new_positions[counter][1]] = 0
-    else:
-        for i in range(counter, len(new_values)):
-            new_values[i] = 0
-        for p in range(counter, len(new_positions)):
-            puzzle[new_positions[p][0]][new_positions[p][1]] = 0
-        counter -= 1
-        new_values[counter] += 1
-
-    if counter == len(new_positions) - 1:
-        puzzle[new_positions[counter][0]][new_positions[counter][1]] = list(digits - set(puzzle[8]))[0]
-        return True, puzzle, new_positions, new_values, counter
-    else:
-        return False, puzzle, new_positions, new_values, counter
-
-
 def sudoku(puzzle):
-    new_positions = get_zeros(puzzle)
-    new_values = [0] * len(new_positions)
+    new_positions = [(i, j) for i in range(9) for j in range(9) if puzzle[i][j] == 0]
+    new_values = [1] * len(new_positions)
     counter = 0
     while True:
-        res, puzzle, new_positions, new_values, counter = solve_sudoku(puzzle, new_positions, new_values, counter)
-        if res:
+        for v in range(new_values[counter], 10):
+            puzzle[new_positions[counter][0]][new_positions[counter][1]] = v
+            if check_puzzle(v, (new_positions[counter][0], new_positions[counter][1])):
+                new_values[counter] = v
+                counter += 1
+                break
+            else:
+                puzzle[new_positions[counter][0]][new_positions[counter][1]] = 0
+        else:
+            for i in range(counter, len(new_values)):
+                new_values[i] = 1
+                puzzle[new_positions[i][0]][new_positions[i][1]] = 0
+            counter -= 1
+            new_values[counter] += 1
+
+        if counter == len(new_positions) - 1:
+            puzzle[new_positions[counter][0]][new_positions[counter][1]] = list({1, 2, 3, 4, 5, 6, 7, 8, 9} - set(puzzle[new_positions[-1][0]]))[0]
+
             return puzzle
 
 
-for row in sudoku(puzzle):
-    print(*row)
+def solve(puzzle):
+    def guessAt():
+        _, x, y = min((len(s), x, y) for x, row in enumerate(grid) for y, s in enumerate(row) if s)
+        return x, y, grid[x][y].pop()
+
+    def isValid():
+        return all(bool(s) ^ bool(ans[x][y]) for x, row in enumerate(grid) for y, s in enumerate(row))
+
+    def filterGridSets(i, j, v):
+        nonlocal counts
+        counts += 1
+        ans[i][j] = v
+        xS, yS = i // 3 * 3, j // 3 * 3
+        for z in range(9):
+            for a, b in ((i, z), (z, j), (xS + z // 3, yS + z % 3)):
+                grid[a][b].discard(v)
+
+    grid = [[{puzzle[x][y]} if puzzle[x][y] else {1, 2, 3, 4, 5, 6, 7, 8, 9} for y in range(9)] for x in range(9)]
+    stk, counts, ans = [], 0, [[0] * 9 for _ in range(9)]
+
+    while counts != 81:
+
+        change = True
+        while change:
+            change = False
+            for x in range(9):
+                for y in range(9):
+                    if len(grid[x][y]) == 1:
+                        change = True
+                        filterGridSets(x, y, grid[x][y].pop())
+
+        if counts == 81: break
+
+        if isValid():
+            x, y, guess = guessAt()
+            stk.append(([[set(s) for s in r] for r in grid], [r[:] for r in ans], x, y, guess, counts))
+            grid[x][y].clear()
+            filterGridSets(x, y, guess)
+        else:
+            grid, ans, x, y, guess, counts = stk.pop()
+            grid[x][y].discard(guess)
+
+    return ans
+
+
+if __name__ == '__main__':
+
+    # puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+    #           [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    #           [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    #           [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    #           [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    #           [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    #           [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    #           [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    #           [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+    puzzle = [[9, 0, 0, 0, 8, 0, 0, 0, 1],
+              [0, 0, 0, 4, 0, 6, 0, 0, 0],
+              [0, 0, 5, 0, 7, 0, 3, 0, 0],
+              [0, 6, 0, 0, 0, 0, 0, 4, 0],
+              [4, 0, 1, 0, 6, 0, 5, 0, 8],
+              [0, 9, 0, 0, 0, 0, 0, 2, 0],
+              [0, 0, 7, 0, 3, 0, 2, 0, 0],
+              [0, 0, 0, 7, 0, 5, 0, 0, 0],
+              [1, 0, 0, 0, 4, 0, 0, 0, 7]]
+
+    for row in solve(puzzle):
+        print(*row)
+
+    # from datetime import datetime
+    # start = datetime.now()
+    # for i in range(100):
+    #     # puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],[6, 0, 0, 1, 9, 5, 0, 0, 0],[0, 9, 8, 0, 0, 0, 0, 6, 0],[8, 0, 0, 0, 6, 0, 0, 0, 3],[4, 0, 0, 8, 0, 3, 0, 0, 1],[7, 0, 0, 0, 2, 0, 0, 0, 6],[0, 6, 0, 0, 0, 0, 2, 8, 0],[0, 0, 0, 4, 1, 9, 0, 0, 5],[0, 0, 0, 0, 8, 0, 0, 7, 9]]
+    #     solve(puzzle)
+    # print(datetime.now() - start)
+
+    # import timeit
+    # print(timeit.timeit("sudoku()", setup="from __main__ import sudoku, check_puzzle"))
